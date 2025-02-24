5087fbff5aaee1d31a9a62dd4e6270c9c237aab9
Oleksii Babaikin <a.babaykin@gmail.com>
2025-02-24 23:21:16 +0100

    Исправил уровень логирования.

---
+++ src/main/java/faang/school/urlshortenerservice/ServiceTemplateApplication.java
@@ -0,0 +1,22 @@
+package faang.school.urlshortenerservice;
+
+import org.springframework.boot.Banner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableCaching
+@EnableRetry
+@EnableScheduling
+@EnableFeignClients
+@SpringBootApplication
+public class ServiceTemplateApplication {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(ServiceTemplateApplication.class)
+                .bannerMode(Banner.Mode.OFF)
+                .run(args);
+    }
+}
